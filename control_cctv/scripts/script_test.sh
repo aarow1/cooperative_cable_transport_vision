606cbef36f07d45a6e155eaea5417e413cf2e094
Aaron Weinstein <aarow@seas.upenn.edu>
2018-07-16 18:30:10 -0400

    switching doesn't seem to be happening

--- control_cctv/scripts/script_test.sh
+++ control_cctv/scripts/script_test.sh
@@ -27,8 +27,14 @@ read -p "set desired payload to 5, 0, 1.0"
 rostopic pub -1 /dragonfly11/control_cctv/payload_cmd geometry_msgs/Pose '{position: {x: 5.0, y: 0.0, z: 1.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}'
 
 
-read -p "set desired payload to 5, 0, 0.0"
-rostopic pub -1 /dragonfly11/control_cctv/payload_cmd geometry_msgs/Pose '{position: {x: 5.0, y: 0.0, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}'
+read -p "set desired payload to 6, 0, 0.0"
+rostopic pub -1 /dragonfly11/control_cctv/payload_cmd geometry_msgs/Pose '{position: {x: 6.0, y: 0.0, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}'
+
+read -p "Press enter to switch controller"
+rostopic pub -1 /$ROBOT/control_cctv/use_cctv_controller std_msgs/Bool 0
+
+read -p "go to 5 0 1.5"
+rosservice call /$ROBOT/mav_services/goTo "goal: [5.0, 0.0, 0.1, 0.0]"
 
 read -p "Press [Enter] to turn off motors"
 rosservice call /$ROBOT/mav_services/motors 0
