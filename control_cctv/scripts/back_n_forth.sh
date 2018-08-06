4a1493e9e5297fc303cb9526b9c4bd7c1c2f01be
Aaron Weinstein <aarow@seas.upenn.edu>
2018-08-06 17:04:19 -0400

    modified multimaster params and back n forth script

--- control_cctv/scripts/back_n_forth.sh
+++ control_cctv/scripts/back_n_forth.sh
@@ -24,17 +24,30 @@ echo "Press the any key to: switch controllers"
 read -rsn1
 rosservice call /dragonfly11/control_cctv/use_cctv_controller "data: true"
 
-echo "Press the any key to set desired payload to 3, 0, 0.6"
-read -rsn1
-rostopic pub -1 /dragonfly11/pl_trackers_manager/line_tracker/goal quadrotor_msgs/LineTrackerGoal "{x: 6.0, y: 0.0, z: 0.3, yaw: 0.0, v_des: 1.0, a_des: 0.0, relative: false}"
+#echo "Press the any key to set line tracker goal to current pos
+#of 2, 0, 0.6"
+#read -rsn1
+#rostopic pub -1 /dragonfly11/pl_trackers_manager/line_tracker/goal quadrotor_msgs/LineTrackerGoal "{x: 2.0, y: 0.0, z: 0.3, yaw: 0.0, v_des: 0.1, a_des: 0.0, relative: false}"
 
-echo "Press the any key to go there"
-read -rsn1
-rosservice call /dragonfly11/pl_trackers_manager/transition pl_trackers/PLLineTracker
+#echo "Press the any key to transition to line tracker"
+#read -rsn1
+#rosservice call /dragonfly11/pl_trackers_manager/transition pl_trackers/PLLineTracker
 
-echo "Press the any key to come back"
-read -rsn1
-rostopic pub -1 /dragonfly11/pl_trackers_manager/line_tracker/goal quadrotor_msgs/LineTrackerGoal "{x: 1.5, y: 0.0, z: 0.2, yaw: 0.0, v_des: 1.0, a_des: 1.0, relative: false}"
+#echo "Press the any key to go out"
+#read -rsn1
+#rostopic pub -1 /dragonfly11/pl_trackers_manager/line_tracker/goal quadrotor_msgs/LineTrackerGoal "{x: 6.0, y: 0.0, z: 0.5, yaw: 0.0, v_des: 0.5, a_des: 0.0, relative: false}"
+
+#echo "Press the any key to right"
+#read -rsn1
+#rostopic pub -1 /dragonfly11/pl_trackers_manager/line_tracker/goal quadrotor_msgs/LineTrackerGoal "{x: 6.0, y: 0.5, z: 0.5, yaw: 0.0, v_des: 0.5, a_des: 0.0, relative: false}"
+
+#echo "Press the any key to left"
+#read -rsn1
+#rostopic pub -1 /dragonfly11/pl_trackers_manager/line_tracker/goal quadrotor_msgs/LineTrackerGoal "{x: 6.0, y: -0.5, z: 0.5, yaw: 0.0, v_des: 0.5, a_des: 0.0, relative: false}"
+
+#echo "Press the any key to come back"
+#read -rsn1
+#rostopic pub -1 /dragonfly11/pl_trackers_manager/line_tracker/goal quadrotor_msgs/LineTrackerGoal "{x: 1.5, y: 0.0, z: 0.2, yaw: 0.0, v_des: 0.25, a_des: 0.0, relative: false}
 
 echo "Press the any key to: turn off motors"
 read -rsn1
